04b83a9e03777ad68cdce152a90823306d812a12
kajun <2587332053@qq.com>
2019-08-15 08:28:34 +0800

    复习基础的wordcount代码

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "sparkReview"
 version := "0.1"
 
 scalaVersion := "2.11.8"
+
+libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "2.3.3"
--- src/main/scala/hellowWord.scala
+++ src/main/scala/hellowWord.scala
@@ -1,5 +1,15 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
 object hellowWord {
   def main(args: Array[String]): Unit = {
-    print("hello")
+    val wordFile = "file:///data/word.txt"
+    val conf = new SparkConf()
+      .setMaster("spark://192.168.1.128:7070").
+      setAppName("wordcount");
+    val sc = new SparkContext(conf)
+    val input = sc.textFile(wordFile, 2).cache()
+    val lines = input.flatMap(line => line.split(" "))
+    val count = lines.map(word => (word, 1)).reduceByKey { case (x, y) => x + y }
+    val output = count.saveAsTextFile("/home/root/wordcout")
   }
 }
